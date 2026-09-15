b7d861cd8d498b08711148f455211ee322492ae5
dbdiaz <dbdiaz0326@gmail.com>
2026-09-15 12:46:15 -0400

    Did work in class

--- csce242/exercises/04javascript/scripts/intro.js
+++ csce242/exercises/04javascript/scripts/intro.js
@@ -1,4 +1,29 @@
+//Shows a message when the button is clicked
 document.getElementById("btn-show-message").onclick = (e) => {
     document.getElementById("p-message").innerHTML = "Hello World";
     e.target.innerHTML = "done!";
+};
+
+//styles the link when it's clicked
+document.getElementById("link").onclick = (e) => {
+    e.preventDefault(); //don't go to links destination
+    e.target.classList.add("cool-link");
+};
+
+//when button clicked make ball bounce
+document.getElementById("btn-bounce").onclick = (e) => {
+    document.getElementById("ball").classList.toggle("bouncing-ball");
 }
+
+
+document.getElementById("txt-num-days").onkeyup = (e) => {
+    const numDays = parseInt(e.target.value);
+    const pMessage = document.getElementById("p-plant-msg");
+    const plantImage = document.getElementById("img-plant");
+    plantImage.classList.remove("hidden");
+}
+
+//counting
+let countInterval;
+let count = 0;
+const pCount = document.getElementById("p-count");
