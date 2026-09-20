24fc58b4fb926ca52d1bb416b66e7bf2c78e3489
dbdiaz <dbdiaz0326@gmail.com>
2026-09-20 16:31:33 -0400

    Finished javascript

--- csce242/assignments/08conditionals/scripts/script.js
+++ csce242/assignments/08conditionals/scripts/script.js
@@ -24,6 +24,31 @@ document.getElementById("exercise-two-link").onclick = (e) => {
     document.getElementById("exercise-two").classList.remove("hidden");
 };
 
+//missing class points
+document.getElementById("txt-days-missed").onkeyup = (e) => {
+    const daysMissed = parseInt(e.target.value);
+    const pointsLost = daysMissed / 25 * 7;
+
+    document.getElementById("deduction-message").innerHTML =
+        `You will lose ${pointsLost.toFixed(1)}% for skipping ${daysMissed} days.`;
+            if(daysMissed == 0) {
+                document.getElementById("attendance-message").innerHTML =
+                    "Perfect attendance! Keep it up.";
+            } else if(daysMissed <= 2) {
+                document.getElementById("attendance-message").innerHTML =
+                    "A couple missed classes should be manageable.";
+            } else if(daysMissed <= 5) {
+                document.getElementById("attendance-message").innerHTML =
+                    "You are starting to miss a noticeable amount of class.";
+            } else if(daysMissed <= 7) {
+                document.getElementById("attendance-message").innerHTML =
+                    "Try not to miss any more classes.";
+            } else {
+                document.getElementById("attendance-message").innerHTML =
+                    "You are missing valuable learning opportunities.";
+            }
+};
+
 //end of semester counter
 const today = new Date();
 const lastDay = new Date(today.getFullYear(), 11, 4);
