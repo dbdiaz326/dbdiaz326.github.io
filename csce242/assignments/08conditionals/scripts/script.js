9aeaf249c016352fcca0b07b6e0cb8fbc3b8f600
dbdiaz <dbdiaz0326@gmail.com>
2026-09-20 15:46:20 -0400

    Allowed travel to and from exercise 1 and 2

--- csce242/assignments/08conditionals/scripts/script.js
+++ csce242/assignments/08conditionals/scripts/script.js
@@ -1,6 +1,5 @@
 //toggle the navigation
 const toggleNav = document.querySelector("#toggle-nav");
-
 toggleNav.onclick = () => {
     document.querySelector("#exercise-nav ul").classList.toggle("hide-small");
 
@@ -10,3 +9,23 @@ toggleNav.onclick = () => {
         toggleNav.innerHTML = "▼";
     }
 }
+
+//show exercise one
+document.getElementById("exercise-one-link").onclick = (e) => {
+    e.preventDefault();
+    document.getElementById("exercise-one").classList.remove("hidden");
+    document.getElementById("exercise-two").classList.add("hidden");
+};
+
+//show exercise two
+document.getElementById("exercise-two-link").onclick = (e) => {
+    e.preventDefault();
+    document.getElementById("exercise-one").classList.add("hidden");
+    document.getElementById("exercise-two").classList.remove("hidden");
+};
+
+//end of semester counter
+const today = new Date();
+const lastDay = new Date(today.getFullYear(), 11, 4);
+const daysLeft = parseInt((lastDay - today) / (1000 * 60 * 60 * 24));
+document.getElementById("days-left").innerHTML = `You have ${daysLeft} days left in the semester`;
