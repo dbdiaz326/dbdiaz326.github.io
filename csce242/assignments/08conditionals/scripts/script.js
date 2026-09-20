9b8fed78c84d4fe30852757ef2a2ac2b1f694064
dbdiaz <dbdiaz0326@gmail.com>
2026-09-20 16:17:09 -0400

    Added javascript for exercise 2

--- csce242/assignments/08conditionals/scripts/script.js
+++ csce242/assignments/08conditionals/scripts/script.js
@@ -29,3 +29,12 @@ const today = new Date();
 const lastDay = new Date(today.getFullYear(), 11, 4);
 const daysLeft = parseInt((lastDay - today) / (1000 * 60 * 60 * 24));
 document.getElementById("days-left").innerHTML = `You have ${daysLeft} days left in the semester`;
+if(daysLeft > 60) {
+    document.getElementById("semester-message").innerHTML = "Plenty of time left in the semester.";
+} else if(daysLeft > 30) {
+    document.getElementById("semester-message").innerHTML = "The semester is moving along.";
+} else if(daysLeft > 7) {
+    document.getElementById("semester-message").innerHTML = "You are in the final stretch.";
+} else {
+    document.getElementById("semester-message").innerHTML = "Almost there!";
+}
